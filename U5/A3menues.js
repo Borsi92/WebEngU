196606f07d04fb3b0fb5c7004d17bfb97a7540ba
Borsi92 <stefanborsutzky@gmx.de>
2018-05-24 23:11:38 +0200

    U5 fertig

---
+++ U5/A3menues.js
@@ -0,0 +1,25 @@
+var menues = {
+    html: {
+        headings: {t: "Headings werden mit dem h-Tag umgesetzt und als Überschriften benutzt."},
+        paragraphs: {t: "Paragraphen werden mit dem p-Tag umgesetzt."},
+        links: {t: "Links werden mit dem h-Tag umgesetzt"},
+        images: {t: "Images werden mit dem img-Tag umgesetzt."},
+        tables: {t: "Tables stellen Tabellen dar."}
+    },
+    css: {
+        selectors: {t: "Mit Selectors können html-Elemente ausgewählt werden."},
+        colors: {t: "Um Elemente zu färben."},
+        boxes: {t: "Jedes HTML-Element wird von CSS-Boxen umhüllt."},
+        display: {t: "Die Art, wie ein html-Element angezeigt wird."},
+        float: {t: "Positionierung."}
+    },
+    javascript: {
+        functions: {t: "Funktionen werden benutzt, um Aufgaben/Algorithmen auszuführen."},
+        objects: {t: "Ermöglichen OOP."},
+        scope: {t: "Bereich, in dem Variablen gültig sind."},
+        events: {t: "Events, die ausgelöst werden und auf die reagiert werden können."},
+        hoisting: {t: "Variablen- und Funktionendeklarationen werden nach oben geschoben."},
+        strict_mode: {t: "Ein strengerer Modus, in dem Fehlerquellen reduziert werden sollen und vermehrt Errors geschmissen werden."},
+        JSON: {t: "JSON ist die JavaScript Object Notation."}
+    }
+};
